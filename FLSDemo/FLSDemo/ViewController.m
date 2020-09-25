d751d7879bfe171908bca10bbdfd1260738fdd6d
李亮 <liang.li1@yintech.cn>
2020-09-25 18:08:00 +0800

    fix

--- FLSDemo/FLSDemo/ViewController.m
+++ FLSDemo/FLSDemo/ViewController.m
@@ -16,6 +16,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(0, 66, self.view.frame.size.width, 66)];
+    label.text = @"ioqioqwiopjkdjcklsjl";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont systemFontOfSize:16];
+    [self.view addSubview:label];
+    
+    
+    
 }
 
 
